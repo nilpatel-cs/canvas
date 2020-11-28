8bc8b0cc65fcaa9fc2ffade531171ead9a9afeac
Nil Patel <nilpatel.cs@gmail.com>
2020-11-27 20:49:27 -0500

    test 2
    window.inner...

--- canvas.js
+++ canvas.js
@@ -1,7 +1,7 @@
 const canvas = document.getElementById('canvas');
 const context = canvas.getContext('2d');
-canvas.width=300;
-canvas.height=200;
+canvas.width=window.innerWidth;
+canvas.height=window.innerHeight;
 
 context.fillStyle = "hotpink";
 context.fillRect(0,0,innerWidth,innerHeight);
