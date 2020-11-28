ec55f728ff4f30be149440b04400beeea5f03f99
Nil Patel <nilpatel.cs@gmail.com>
2020-11-27 20:42:11 -0500

    test 1

---
+++ canvas.js
@@ -0,0 +1,7 @@
+const canvas = document.getElementById('canvas');
+const context = canvas.getContext('2d');
+canvas.width=300;
+canvas.height=200;
+
+context.fillStyle = "hotpink";
+context.fillRect(0,0,innerWidth,innerHeight);
